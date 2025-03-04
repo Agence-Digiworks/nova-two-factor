416fd6ae71e710a3fbd052fff641b4679db99d4a
Sonia <sonia@digiworks.fr>
2025-03-04 10:12:11 +0100

    clear button

--- resources/js/components/Clear.vue
+++ resources/js/components/Clear.vue
@@ -18,7 +18,8 @@
                     </div>
 
                     <DefaultButton 
-                    class="shrink-0 h-9 px-4 focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring text-white dark:text-gray-800 inline-flex items-center font-bold shadow rounded focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm tw-cursor-pointer" type="submit">
+                    class="shrink-0 h-9 px-4 focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring text-white dark:text-gray-800 inline-flex items-center font-bold shadow rounded focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm tw-cursor-pointer" type="submit"
+                    @click="auth">
                         <span>
                             {{ __('Clear') }}
                         </span>
